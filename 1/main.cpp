8f6ae2d0ce8f3a2903d5a7ff23280db3c1a81d24
Torsten Gustafsson <torstengustafsson1992@gmail.com>
2018-12-01 10:53:46 +0100

    add day 1 part 2

--- 1/main.cpp
+++ 1/main.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <fstream>
+#include <vector>
+#include <map>
 
-int main() {
-    std::ifstream ifs(std::string(AOC1DIR) + "/input.txt");
+void clear(std::ifstream& ifs) {
+    ifs.clear();                 // clear fail and eof bits
+    ifs.seekg(0, std::ios::beg); // back to the start!
+}
+
+int calculatePart1(std::ifstream& ifs) {
     int res = 0;
 
     char sign;
@@ -11,5 +17,41 @@ int main() {
         res += sign == '+' ? val : -val;
     }
 
-    std::cout << "result is = " << res << std::endl;
+    return res;
+}
+
+int calculatePart2(std::ifstream& ifs) {
+    int res;
+    std::vector<int> vals;
+
+    // read values
+    char sign;
+    int val;
+    while (ifs >> sign >> val) {
+        vals.push_back(sign == '+' ? val : -val);
+    }
+
+    // find first repeating frequency
+    bool found = false;
+    int currFreq = 0;
+    std::map<int, int> valmap;
+    while (!found) {
+        for (int val : vals) {
+            currFreq += val;
+            if (++valmap[currFreq] > 1) {
+                found = true;
+                res = currFreq;
+                break;
+            }
+        }
+    }
+
+    return res;
+}
+
+int main() {
+    std::ifstream ifs(std::string(AOC1DIR) + "/input.txt");
+    std::cout << "part 1 result is = " << calculatePart1(ifs) << std::endl;
+    clear(ifs);
+    std::cout << "part 2 result is = " << calculatePart2(ifs) << std::endl;
 }
