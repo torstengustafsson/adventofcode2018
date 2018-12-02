8c0f742ff6f8c0e866b99b078f93d7b858481418
Torsten Gustafsson <torstengustafsson1992@gmail.com>
2018-12-02 11:29:36 +0100

    change input reading to avoid reading it twice
    Note: further optimisation by calculating part 1 and part 2
    together could be done, but we keep them separate for readability.

--- day1/day1.cpp
+++ day1/day1.cpp
@@ -3,33 +3,30 @@
 #include <vector>
 #include <map>
 
-void clear(std::ifstream& ifs) {
-    ifs.clear();                 // clear fail and eof bits
-    ifs.seekg(0, std::ios::beg); // back to the start!
-}
+std::vector<int> readInput() {
+    std::vector<int> res;
 
-int calculatePart1(std::ifstream& ifs) {
-    int res = 0;
-
+    std::ifstream ifs(std::string(ROOT_DIR) + "/day1/input.txt");
     char sign;
     int val;
     while (ifs >> sign >> val) {
-        res += sign == '+' ? val : -val;
+        res.push_back(sign == '+' ? val : -val);
+    }
+    return res;
+}
+
+int calculatePart1(std::vector<int> vals) {
+    int res = 0;
+
+    for(auto val : vals) {
+        res += val;
     }
 
     return res;
 }
 
-int calculatePart2(std::ifstream& ifs) {
+int calculatePart2(std::vector<int> vals) {
     int res;
-    std::vector<int> vals;
-
-    // read values
-    char sign;
-    int val;
-    while (ifs >> sign >> val) {
-        vals.push_back(sign == '+' ? val : -val);
-    }
 
     // find first repeating frequency
     bool found = false;
@@ -50,8 +47,7 @@ int calculatePart2(std::ifstream& ifs) {
 }
 
 int main() {
-    std::ifstream ifs(std::string(ROOT_DIR) + "/day1/input.txt");
-    std::cout << "part 1 result is = " << calculatePart1(ifs) << std::endl;
-    clear(ifs);
-    std::cout << "part 2 result is = " << calculatePart2(ifs) << std::endl;
+    auto vals = readInput();
+    std::cout << "part 1 result is = " << calculatePart1(vals) << std::endl;
+    std::cout << "part 2 result is = " << calculatePart2(vals) << std::endl;
 }
--- day2/day2.cpp
+++ day2/day2.cpp
@@ -6,23 +6,36 @@
 
 const int LETTERS = 26;
 
-int calculatePart1(std::ifstream& ifs) {
+std::vector<std::string> readInput() {
+    std::vector<std::string> res;
+
+    std::ifstream ifs(std::string(ROOT_DIR) + "/day2/input.txt");
+    std::string line;
+    while (std::getline(ifs, line)) {
+        res.push_back(line);
+    }
+    return res;
+}
+
+int calculatePart1(std::vector<std::string> lines) {
     int twices = 0;
     int thrices = 0;
 
-    std::string line;
-    while (std::getline(ifs, line)) {
+    for (auto line : lines) {
         int charmap[LETTERS] = { 0 }; // storage for lower case ascii letters
         bool twice = false;
         bool thrice = false;
+
         for (auto ch : line) {
             charmap[ch - 97]++; // we know input only contains lower case charactes (ascii 97-122)
         }
+
         for (int i = 0; i < LETTERS; i++) {
             if (charmap[i] == 2) { twice = true; }
             if (charmap[i] == 3) { thrice = true; }
         }
-        twices += twice; // 0 or 1
+
+        twices += twice;   // 0 or 1
         thrices += thrice;
     }
 
@@ -30,8 +43,8 @@ int calculatePart1(std::ifstream& ifs) {
 }
 
 int main() {
-    std::ifstream ifs(std::string(ROOT_DIR) + "/day2/input.txt");
-    std::cout << "part 1 result is = " << calculatePart1(ifs) << std::endl;
+    auto lines = readInput();
+    std::cout << "part 1 result is = " << calculatePart1(lines) << std::endl;
 
     return 0;
 }
